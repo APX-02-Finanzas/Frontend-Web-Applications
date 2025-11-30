a18938550ec4b24af168a8808de588eba00841e7
loreleysarmiento <sarmientoloreley@gmail.com>
2025-11-30 14:27:41 -0500

    Environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  serverBaseUrl: "https://backend-web-services-zj3w.onrender.com",
+  serverBaseUrl: "https://backend-web-services-zj3w.onrender.com/api/v1",
   authenticationEndpointPath: '/authentication',
   usersEndpointPath: '/users',
   clientsEndpointPath: '/clients',
